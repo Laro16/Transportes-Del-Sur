ac4478b59eac958bd8a3861ead3bd8e2d39c5dd8
Laro16 <luis21aro@gmail.com>
2026-05-13 12:16:55 -0600

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://upqjiwliadhlhcewqoou.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_cnPfojHJI5L9S70QSs2a8g_IjRcxmVE";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);

a359076234ce802ffe19dbceae433b64bb89414e
Laro16 <luis21aro@gmail.com>
2026-05-13 15:06:08 -0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,10 @@
 const SUPABASE_URL = "https://upqjiwliadhlhcewqoou.supabase.co";
 
+// Nota: Esta es tu llave pública (anon key). Es seguro usarla en el frontend.
 const SUPABASE_KEY = "sb_publishable_cnPfojHJI5L9S70QSs2a8g_IjRcxmVE";
 
-const supabaseClient = window.supabase.createClient(
+// EXPLICITAMENTE asignamos el cliente a "window" para que proyecto.jsx lo pueda leer
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
